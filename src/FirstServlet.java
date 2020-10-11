c7883d2a0d24ae65a7cf6c439bf3955e32b78be8
Oleh_Strykun <alik49410@gmail.com>
2020-10-11 20:40:53 +0300

    1

--- src/FirstServlet.java
+++ src/FirstServlet.java
@@ -1,16 +1,19 @@
 import logic.Cart;
 
 import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
+import javax.servlet.http.*;
 import java.io.PrintWriter;
 
-public class FirstServlet extends javax.servlet.http.HttpServlet {
-    protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+public class FirstServlet extends HttpServlet {
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws javax.servlet.ServletException, IOException {
 
     }
 
-    protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
         HttpSession session = request.getSession();
 
